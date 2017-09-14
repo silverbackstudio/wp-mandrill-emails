7e731971425ebead3bd2915a7a9b29f59663ef85
Brando Meniconi <amministrazione@silverbackstudio.it>
2017-09-14 16:17:55 +0000

    fixed merge_tags routine

--- src/RCP_Emails.php
+++ src/RCP_Emails.php
@@ -157,7 +157,7 @@ class RCP_Emails extends \RCP_Emails {
 				var_dump($this->member_id);
                 $merge_tags[ $email_tag['tag'] ] = array(
         			'name' => strtoupper( $email_tag['tag'] ),
-        			'content' => call_user_func( $email_tag['function'], $this->member_id, $this->payment_id, $email_tag ),
+        			'content' => call_user_func( $email_tag['function'], $this->member_id, $this->payment_id, $email_tag['tag'] ),
     			);		    
 			}
 		}
